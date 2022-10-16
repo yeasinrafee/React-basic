085a0bb1cee3c7562f0a5fd38e82508043d80ce1
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-17 00:01:52 +0600

    Function as props

--- react-basics/src/components/Modal.js
+++ react-basics/src/components/Modal.js
@@ -1,11 +1,12 @@
 import './Modal.css';
 
-export default function Modal({children}) {
+export default function Modal({children, showModal}) {
   return (
     <div>
         <div className="modal-backdrop">
             <div className="modal">
                 {children}
+                <button onClick={()=> showModal(false)}>Close</button>
             </div>
         </div>
     </div>
