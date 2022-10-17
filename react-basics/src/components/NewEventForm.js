8636ec71a87fd1206d2da2355696a7a3fe54b996
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-17 12:48:01 +0600

    Controlled Input

--- react-basics/src/components/NewEventForm.js
+++ react-basics/src/components/NewEventForm.js
@@ -1,17 +1,35 @@
+import { useState } from 'react';
 import './NewEventForm.css';
 
 export default function NewEventForm() {
+    const [title, setTitle] = useState('');
+    const [date, setDate] = useState('');
+
+    const handleReset = () =>{
+        setTitle('');
+        setDate('');
+    }
+
   return (
     <form className="new-event-form">
         <label>
             <span>Event Title:</span>
-            <input type="text" />
+            <input 
+                type="text" 
+                onChange={(e) => setTitle(e.target.value)}
+                value= {title}
+                />
         </label>
         <label>
             <span>Event Date:</span>
-            <input type="date" />
+            <input 
+                type="date" 
+                onChange={(e) => setDate(e.target.value)}
+                value = {date}
+                />
         </label>
-        <button>Submit</button>
+        <button style={{marginRight: "20px"}}>Submit</button>
+        <button onClick={handleReset}>Reset</button>
     </form>
   )
 }
